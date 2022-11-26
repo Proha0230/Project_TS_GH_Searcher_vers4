2e78cf171c4a13efee39408dcd6f5f1e626dcb37
Ivan <Priest.ivan14@gmail.com>
2022-11-26 08:49:53 +0300

    Третья версия приложения GitHub Finder. Добавлена Шапка Профиля с фото,логином,именем, датой создания аккаунта пользователя (UserTitle). Добавлено bio пользователя, если он не заполнял его то выдаст надпись This profile has no bio

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,4 +1,5 @@
 import { UserStat } from 'components/UserStat';
+import { UserTitle } from 'components/UserTitle';
 import { LocalGithubUser } from 'types';
 import styles from './UserCard.module.scss';
 
@@ -7,19 +8,28 @@ interface UserCardProps extends LocalGithubUser {}
 export const UserCard = (props: UserCardProps) => {
   return(
   <div className={styles.userCard}>
+   
    <img 
    src={props.avatar}
    alt={props.login}
   className={styles.avatar}
    />
+   
+   <UserTitle 
+   name={props.name} 
+   login={props.login} 
+   created={props.created} />
+   
    <UserStat
    repos={props.repos}
    followers={props.followers}
    following={props.following}
    />
+   
    <p className={`${styles.bio}${props.bio ? "" : `${styles.empty}`}`}>
     {props.bio || "This profile has no bio"}
    </p>
+  
   </div>
  )
 };
